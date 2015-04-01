7927d0a685d6ac684f6e65ce26652c74cdeb6057
adamedx <adamed@getchef.com>
2015-04-01 10:37:03 -0700

    Correctly set meta configuration

--- Berksfile
+++ Berksfile
@@ -1,3 +1,5 @@
 source "https://supermarket.chef.io"
 
 metadata
+
+cookbook 'powershell', git: 'https://github.com/opscode-cookbooks/powershell'
--- recipes/default.rb
+++ recipes/default.rb
@@ -4,14 +4,22 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 
-include_recipe 'powershell::powershell5'
+# include_recipe 'powershell::powershell5'
 include_recipe 'powershell::dsc'
 
-dsc_script 'disable_lcm' do
+powershell_script 'disable_lcm' do
   code <<-EOH
+Configuration 'disable_lcm' {
+  node 'localhost' {
     LocalConfigurationManager {
       RefreshMode = 'Disabled'
-   }
+    }
+  }
+}
+
+disable_lcm
+
+set-dsclocalconfigurationmanager -path .\disable_lcm -wait
 EOH
 end
 
